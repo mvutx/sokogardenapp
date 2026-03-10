5c8c3ce39c5020cdcb626537d887914876c839a2
vutii <giftmuuo.com@gmail.com>
2026-03-10 14:09:33 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Getproducts from './components/Getproducts';
+import Addproducts from './components/Addproducts';
+import Signin from './components/Signin';
+import Notfound from './components/Notfound';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+   <Router>
+     <div className="App">
+      <header className="App-header">
+       <h1>welcome to Sokogarden</h1>
+      </header>
+      {/*Below is our different routes together with the rendered components*/}
+      <Routes>
+        <Route path='/' element={<Getproducts />} />
+        <Route path='/addproducts' element={<Addproducts />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='*' element={<Notfound />} />
+        
+      </Routes>
+    </div>
+   </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Signup.jsx
@@ -0,0 +1,12 @@
+// type rafce
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Welcome to the Signup page</h1>
+    </div>
+  )
+}
+
+export default Signup;
